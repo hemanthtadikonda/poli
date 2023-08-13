6a63951f37e325beea3b088ca8ca2a18c96022af
hemanth <hemanthtadikonda29@gmail.com>
2023-08-13 12:13:48 +0530

    server configuration

--- server.tf
+++ server.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "instance" {
 
-  ami                 = data.aws_ami.ami.id
-  instance_tpe        = "t2.micro"
-  vpc_security_groups = ["sg-0baa986d8ebb5eeda"]
+  ami                      = data.aws_ami.ami.id
+  instance_type            = "t2.micro"
+  vpc_security_group_ids  = ["sg-0baa986d8ebb5eeda"]
 
   tags = {
     Name = "chocolux-server"
