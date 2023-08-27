8dae4c0557e3d3c9fe3b7f5b20117e8bfc2b8f33
hemanth <hemanthtadikonda29@gmail.com>
2023-08-27 09:08:15 +0530

    terraform with ansible

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "allow_all" {
 
   ingress {
     description      = "WEB"
-    from_port        = [80]
-    to_port          = [80]
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
