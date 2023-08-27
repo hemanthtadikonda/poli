d88de6e106a85ca8b4350844deda3fddcf8aef44
hemanth <hemanthtadikonda29@gmail.com>
2023-08-27 09:07:22 +0530

    terraform with ansible

--- main.tf
+++ main.tf
@@ -13,13 +13,19 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "ALLOW-ALL"
-    from_port        = [0 - 65535]
-    to_port          = [0 - 65535]
+    description      = "WEB"
+    from_port        = [80]
+    to_port          = [80]
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
